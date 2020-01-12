06c21e9d819bfdd62ff7d1c0d74e1627d96ba5a3
gg-mg <ggichuki2017@gmail.com>
2020-01-12 22:43:22 +0300

    Removed the XHTML plain text

--- page1.php
+++ page1.php
@@ -1,5 +1,5 @@
 <?php echo file_get_contents("html/header1.html"); ?>
 <title>Page 2</title>
 <?php echo file_get_contents("html/header2.html"); ?>
-This is page 2, the home page.
+This is page 2.
 <?php echo file_get_contents("html/footer.html"); ?>
--- page2.php
+++ page2.php
@@ -3,5 +3,5 @@
 <?php echo file_get_contents("html/header1.html"); ?>
 <title>Page 3</title>
 <?php echo file_get_contents("html/header2.html"); ?>
-This is page 3, the home page.
+This is page 3.
 <?php echo file_get_contents("html/footer.html"); ?>
