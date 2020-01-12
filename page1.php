64e3074fe94ecf16d0098de40fc350e64225aae0
gg-mg <ggichuki2017@gmail.com>
2020-01-12 22:38:04 +0300

    Initial commit

---
+++ page1.php
@@ -0,0 +1,5 @@
+<?php echo file_get_contents("html/header1.html"); ?>
+<title>Page 2</title>
+<?php echo file_get_contents("html/header2.html"); ?>
+This is page 2, the home page.
+<?php echo file_get_contents("html/footer.html"); ?>
---
+++ page2.php
@@ -0,0 +1,7 @@
+
+
+<?php echo file_get_contents("html/header1.html"); ?>
+<title>Page 3</title>
+<?php echo file_get_contents("html/header2.html"); ?>
+This is page 3, the home page.
+<?php echo file_get_contents("html/footer.html"); ?>
